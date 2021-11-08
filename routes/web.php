264287e18c0feeb5dbf87470314dd026ed4db6a6
yusei <yut06002fight@gmail.com>
2021-11-08 07:22:05 +0000

    kadai

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'NewsController@index');
+Route::get('admin/profile/create',' ProfileController@add');
+Route::get('admin/profile/edit','ProfileController@edit');
